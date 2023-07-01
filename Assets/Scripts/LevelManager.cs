5d3aff50b3d5098a2fbd9cf0d5b12ccd65658870
jukeboxplays <jukebox.plays@gmail.com>
2023-07-01 12:52:36 -0300

    Big UPDATE - LAST COMMIT BEFORE GAMECON

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.UI;
 using TMPro;
 
 public class LevelManager : MonoBehaviour
@@ -13,6 +14,11 @@ public class LevelManager : MonoBehaviour
 
     public LevelProgressUI levelProgressBar;
     public TMP_Text levelProgressText;
+
+    public Button levelSwitch_Left;
+    public Button levelSwitch_Right;
+
+    public TMP_Text currentLevelDisplay;
 
     //TEMPORARIO PARA MOSTRAR MOCKUP NO EVENTO
     public List<LevelData> allLevels;
@@ -32,6 +38,9 @@ public class LevelManager : MonoBehaviour
     void Start()
     {
         currentLevel = allLevels[0];
+        FlushLevelArrows();
+        DisplayLevelName();
+
         clickAnimation.currentInDisplay = currentLevel.GetComponent<LevelAnimation>().levelOnClickAnimations;
     }
 
@@ -40,7 +49,7 @@ public class LevelManager : MonoBehaviour
         //NAO PRECISARA SER NO UPDATE, DEPOIS DO EVENTO MUDAR PARA ACONTECIMENTOS DE ACOES. ESTUDAR SE EVENTOS PODEM SER UMA BOA IDEIA
         //(POIS CONECTAR TODAS AS ENTRADAS DE PONTOS EM UMA ACTION SO TALVEZ, SEM NECESSITAR FAZER A OPERACAO TODO O FRAME)
         levelProgressBar.image.fillAmount = Mathf.Lerp(0,1, currentLevel.currentCumulative/currentLevel.cumulativeGoal);
-        levelProgressText.text = $"{currentLevel.currentCumulative} / {currentLevel.cumulativeGoal}";
+        levelProgressText.text = $"{currentLevel.currentCumulative.ToString("F0")} / {currentLevel.cumulativeGoal.ToString("F0")}";
     }
 
 
@@ -80,14 +89,48 @@ public class LevelManager : MonoBehaviour
         SwitchCameraByPriority(currentLevel.virtualCamera, targetLevel.virtualCamera);
 
         currentLevel = targetLevel;
+        FlushLevelArrows();
+        DisplayLevelName();
+
         var levelAnimation = currentLevel.GetComponent<LevelAnimation>();
 
         clickAnimation.currentInDisplay = levelAnimation.levelOnClickAnimations;
     }
 
+    //public void UISwitchLevelHandler(GameObject originButton, bool next)
+
+
+    //PROVISORIO PARA EVENTO
+    public void DisplayLevelName()
+    {
+        currentLevelDisplay.text = currentLevel.gameObject.name;
+    }
+
+
+    public void FlushLevelArrows()
+    {
+        int levelCount = allLevels.Count - 1;
+        int currentLevelIndex = allLevels.IndexOf(currentLevel);
+        //Button arrow;
+
+        if (currentLevelIndex >= levelCount|| !currentLevel.isConcluded)
+        {
+            levelSwitch_Right.gameObject.SetActive(false);
+        }
+        else levelSwitch_Right.gameObject.SetActive(true);
+
+        if (currentLevelIndex <= 0)
+        {
+            levelSwitch_Left.gameObject.SetActive(false);
+        }
+        else levelSwitch_Left.gameObject.SetActive(true);
+        //arrow.gameObject.SetActive(arrow.gameObject.activeSelf);
+    }
+
+
     public void UISwitchLevelHandler(bool next)
     {
-        var currentLevelIndex = allLevels.IndexOf(currentLevel);
+        int currentLevelIndex = allLevels.IndexOf(currentLevel);
         int targetLevelIndex = 0;
 
         if (next)
@@ -95,6 +138,9 @@ public class LevelManager : MonoBehaviour
             if (currentLevelIndex >= allLevels.Count) return;
 
             targetLevelIndex = currentLevelIndex + 1;
+
+            var targetLevel = allLevels[targetLevelIndex];
+//            if (!targetLevel.isConcluded) originButton.SetActive(false);
         }
 
         else
@@ -103,6 +149,9 @@ public class LevelManager : MonoBehaviour
 
             targetLevelIndex = currentLevelIndex -1;
         }
+
+
+
 
         SwitchLevel(allLevels[targetLevelIndex]);
     }
