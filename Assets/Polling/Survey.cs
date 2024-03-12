a59e817a12d37547b96806b09ee54d898b5d87cf
Carlos Rodrigues <jukebox.plays@gmail.com>
2024-03-12 19:56:47 -0300

    Survey - SDK wrapper

--- Assets/Polling/Survey.cs
+++ Assets/Polling/Survey.cs
@@ -1,8 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class Survey
-{
+namespace Polling { 
 
+    public enum ViewType
+    {
+        None,
+        Dialog,
+        Bottom
+    }
+
+    public class CallbackHandler
+    {
+        public Action onSuccess;
+        public Action onFailure;
+        public AndroidJavaObject callbackHandler;
+
+        public CallbackHandler(GameObject target, Action onSuccess, Action onFailure)
+        {
+            var callbackHandler = new AndroidJavaObject("com.polling.sdk.UnityCallbackHandler", target.name, onSuccess.ToString(), onFailure.ToString());
+
+            this.onSuccess = onSuccess;
+            this.onFailure = onFailure;
+
+            this.callbackHandler = callbackHandler;
+        }
+    }
+
+
+    public class Survey
+    {
+
+        private AndroidJavaObject survey;
+
+        public Survey(RequestIdentification requestIdentification, CallbackHandler callbackHandler)
+        {
+            survey = new AndroidJavaObject("com.polling.sdk.Survey", requestIdentification, callbackHandler.callbackHandler);
+        }
+
+        public void AvailableSurveys()
+        {
+            survey.Call("availableSurveys");
+        }
+
+        public void AvailableSurveys(ViewType viewType)
+        {
+            survey.Call("availableSurveys", Bridge.UnityActivity(), viewType);
+        }
+
+
+        public void SingleSurvey(string surveyId)
+        {
+            survey.Call("singleSurvey", surveyId);
+        }
+
+        public void SingleSurvey(string surveyId, ViewType viewType)
+        {
+            survey.Call("singleSurvey", surveyId, Bridge.UnityActivity(), viewType);
+        }
+
+
+        public void CompletedSurveys()
+        {
+            survey.Call("completedSurveys");
+        }
+    }
 }
