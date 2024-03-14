037ca3d4fab62bd88aeb00d71671ab0b98788595
Carlos Rodrigues <jukebox.plays@gmail.com>
2024-03-14 16:53:01 -0300

    Reward implementation

--- Assets/Polling/CallbackHandler.cs
+++ Assets/Polling/CallbackHandler.cs
@@ -7,16 +7,20 @@ namespace Polling
 {
     public class CallbackHandler
     {
+        public GameObject gameObject;
         public Action<string> onSuccess;
         public Action<string> onFailure;
+        public Action<string> onReward;
         public AndroidJavaObject callbackHandler;
 
-        public CallbackHandler(GameObject target, Action<string> onSuccess, Action<string> onFailure)
+        public CallbackHandler(GameObject target, Action<string> onSuccess, Action<string> onFailure, Action<string> onReward)
         {
             var callbackHandler = new AndroidJavaObject("com.polling.sdk.UnityCallbackHandler", target.name, onSuccess.Method.Name, onFailure.Method.Name);
 
+            this.gameObject = target;
             this.onSuccess = onSuccess;
             this.onFailure = onFailure;
+            this.onReward = onReward;
 
             this.callbackHandler = callbackHandler;
         }
--- Assets/Polling/Survey.cs
+++ Assets/Polling/Survey.cs
@@ -15,10 +15,14 @@ namespace Polling {
     {
 
         private AndroidJavaObject survey;
-
+        private CallbackHandler callbackHandler;
+        private CallbackHandler rewardCallback;
         public Survey(Identifier identifier, CallbackHandler callbackHandler)
         {
             survey = new AndroidJavaObject("com.polling.sdk.Survey", identifier.requestIdentification, callbackHandler.callbackHandler);
+            this.callbackHandler = callbackHandler;
+
+            this.rewardCallback = new CallbackHandler(callbackHandler.gameObject, callbackHandler.onReward, callbackHandler.onFailure, callbackHandler.onReward);
         }
 
         public void AvailableSurveys()
@@ -28,6 +32,7 @@ namespace Polling {
 
         public void AvailableSurveys(ViewType viewType)
         {
+            RewardSetup();
             survey.Call("availableSurveys", Bridge.UnityActivity(), viewType.ToString());
         }
 
@@ -39,6 +44,7 @@ namespace Polling {
 
         public void SingleSurvey(string surveyId, ViewType viewType)
         {
+            RewardSetup();
             survey.Call("singleSurvey", surveyId, Bridge.UnityActivity(), viewType.ToString());
         }
 
@@ -49,39 +55,49 @@ namespace Polling {
         }
 
 
-        private void OnReward(string response)
+        private void RewardSetup()
         {
+            var currentCallback = survey.Get<AndroidJavaObject>("callbackHandler");
+            if (currentCallback == rewardCallback.callbackHandler)
+            {
+                Debug.Log("RewardSetup - Same callback for rewards. Skipping...");
+                return;
+            }
+            else
+            {
+                Debug.Log("RewardSetup - Not the same callback for rewards. Updating...");
+            }
+
+            survey.Call("updateCallbacks", rewardCallback.callbackHandler);
+            Debug.Log("Updated!");
+        }
+
+
+        public List<Reward> OnReward(string response)
+        {
+
+            Debug.Log("OnReward running.");
+
             //List<Dictionary<string, string>> surveys = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(response);
             RewardList rewardList = new();
-            List<Reward> rewards = rewardList.DeserializeSurveys(response);
+            List<Reward> rewards = DeserializeSurveys(response);
 
             foreach (Reward reward in rewards) 
             {
-                Debug.Log($"{reward.reward_name} | {reward.reward_amount}");
+                Debug.Log($"Unity Reward: {reward.reward_name} | {reward.reward_amount}");
             }
 
+            survey.Call("updateCallbacks", callbackHandler.callbackHandler);
+            
+            return rewards;
         }
-    }
 
-    class Reward
-    {
-        public string completed_at;
-        public string reward_amount;
-        public string reward_name;
-        public string name;
-        public string started_at;
-        public string uuid;
-    }
-
-    class RewardList
-    {
-        public List<Reward> rewards;
 
         public List<Reward> DeserializeSurveys(string jsonArray)
         {
-            string json = "{\"surveys\":" + jsonArray + "}";
-            rewards = JsonUtility.FromJson<List<Reward>>(json);
-            return rewards;
+            string json = "{\"rewards\":" + jsonArray + "}";
+            var rewards = JsonUtility.FromJson<RewardList>(json);
+            return rewards.rewards;
         }
     }
 }
