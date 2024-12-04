a1c9474184dd3b0cfe377c660fffa942fd846f1c
Carlos Rodrigues <jukebox.plays@gmail.com>
2024-12-04 18:02:27 -0300

    Change to custom

--- Assets/Scripts/PollingHandlerCustom.cs
+++ Assets/Scripts/PollingHandlerCustom.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PollingHandler : MonoBehaviour
+public class PollingHandlerCustom : MonoBehaviour
 {
 
     public Animator pollingButton;
